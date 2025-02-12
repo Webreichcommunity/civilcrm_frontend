dda32dbaf812d4bf1fa2ef539b0f187a2da3ea7e
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2025-02-12 14:39:34 +0530

    some demo update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-bold'>update</div>
+    <div className='bg-red-400 text-bold'>Action Bazz and Sanskruti chandu Red Bondre</div>
   )
 }
 
